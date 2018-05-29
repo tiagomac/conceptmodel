8e5857e1cc2a7390ea0056aaceaae0f652aebd40
Tiago Mesquita <tiagomac@gmail.com>
2018-05-29 18:27:41 -0300

    Criando operação da instanciação

--- src/main/java/com/tiagomac/ConceptmodelApplication.java
+++ src/main/java/com/tiagomac/ConceptmodelApplication.java
@@ -1,12 +1,30 @@
 package com.tiagomac;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.tiagomac.domain.Categoria;
+import com.tiagomac.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ConceptmodelApplication {
+public class ConceptmodelApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConceptmodelApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
