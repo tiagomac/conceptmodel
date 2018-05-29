efcc3d236e4692bd21f5ee99d46adfd2744f9041
Tiago Mesquita <tiagomac@gmail.com>
2018-05-29 18:47:53 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/tiagomac/ConceptmodelApplication.java
+++ src/main/java/com/tiagomac/ConceptmodelApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.tiagomac.domain.Categoria;
+import com.tiagomac.domain.Produto;
 import com.tiagomac.repositories.CategoriaRepository;
+import com.tiagomac.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ConceptmodelApplication implements CommandLineRunner {
@@ -16,6 +18,9 @@ public class ConceptmodelApplication implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ConceptmodelApplication.class, args);
 	}
@@ -24,7 +29,17 @@ public class ConceptmodelApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800d);
+		Produto p3 = new Produto(null, "Mouse", 80d);
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 }
