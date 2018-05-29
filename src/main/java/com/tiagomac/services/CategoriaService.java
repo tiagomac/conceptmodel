5de814cda77bd8c345cdf7de42a65b7b1bf58b5f
Tiago Mesquita <tiagomac@gmail.com>
2018-05-29 18:21:33 -0300

    criado repository e service para categoria

---
+++ src/main/java/com/tiagomac/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.tiagomac.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tiagomac.domain.Categoria;
+import com.tiagomac.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		
+	}
+}
