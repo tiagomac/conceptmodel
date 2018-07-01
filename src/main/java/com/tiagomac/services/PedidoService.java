d025681cf3540167d5fe53ddcbcd8c56fb931eb1
Tiago Mesquita <tiagomac@gmail.com>
2018-07-01 14:17:49 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/tiagomac/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.tiagomac.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tiagomac.domain.Pedido;
+import com.tiagomac.repositories.PedidoRepository;
+import com.tiagomac.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! ID: " + id + ", Tipo: " + Pedido.class.getName()));
+
+	}
+}
