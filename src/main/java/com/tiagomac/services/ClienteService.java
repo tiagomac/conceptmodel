004acba0c291e6fff710b86a840037fba56d32bc
Tiago Mesquita <tiagomac@gmail.com>
2018-07-01 12:21:36 -0300

    Endpoint /clientes/{id} disponível

---
+++ src/main/java/com/tiagomac/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.tiagomac.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tiagomac.domain.Cliente;
+import com.tiagomac.repositories.ClienteRepository;
+import com.tiagomac.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! ID: " + id + ", Tipo: " + Cliente.class.getName()));
+
+	}
+}
