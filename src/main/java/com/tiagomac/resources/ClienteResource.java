5dbb901c8d041f5ed18ec44c551e226b7cd26633
Tiago Mesquita <tiagomac@gmail.com>
2018-07-12 13:00:01 -0300

    inserindo um novo cliente com POST

--- src/main/java/com/tiagomac/resources/ClienteResource.java
+++ src/main/java/com/tiagomac/resources/ClienteResource.java
@@ -19,6 +19,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.tiagomac.domain.Cliente;
 import com.tiagomac.dto.ClienteDTO;
+import com.tiagomac.dto.ClienteNewDTO;
 import com.tiagomac.services.ClienteService;
 
 @RestController
@@ -34,14 +35,13 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(obj);
 	}
 
-	// @RequestMapping(method=RequestMethod.POST)
-	// public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO objDto){
-	// Cliente obj = service.fromDTO(objDto);
-	// obj = service.insert(obj);
-	// URI uri =
-	// ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-	// return ResponseEntity.created(uri).build();
-	// }
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
--- src/main/java/com/tiagomac/services/ClienteService.java
+++ src/main/java/com/tiagomac/services/ClienteService.java
@@ -9,10 +9,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.tiagomac.domain.Cidade;
 import com.tiagomac.domain.Cliente;
+import com.tiagomac.domain.Endereco;
+import com.tiagomac.domain.enums.TipoCliente;
 import com.tiagomac.dto.ClienteDTO;
+import com.tiagomac.dto.ClienteNewDTO;
 import com.tiagomac.repositories.ClienteRepository;
+import com.tiagomac.repositories.EnderecoRepository;
 import com.tiagomac.services.exceptions.DataIntegrityException;
 import com.tiagomac.services.exceptions.ObjectNotFoundException;
 
@@ -22,11 +28,28 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! ID: " + id + ", Tipo: " + Cliente.class.getName()));
 
+	}
+
+	/**
+	 * @Transactional - garante que o cliente e seus endereços serão salvos na mesma
+	 *                transação
+	 * @param obj
+	 * @return Cliente
+	 */
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
 	}
 
 	public Cliente update(Cliente obj) {
@@ -55,6 +78,23 @@ public class ClienteService {
 
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
+				TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
 	}
 
 	private void updateData(Cliente newObj, Cliente obj) {
