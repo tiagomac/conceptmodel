12029e48b13690edc76be406ecabded500bf9d5b
Tiago Mesquita <tiagomac@gmail.com>
2018-07-14 19:12:39 -0300

    Validacao customizada: email nao repetido na insercao de Cliente

--- src/main/java/com/tiagomac/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/tiagomac/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tiagomac.domain.Cliente;
 import com.tiagomac.domain.enums.TipoCliente;
 import com.tiagomac.dto.ClienteNewDTO;
+import com.tiagomac.repositories.ClienteRepository;
 import com.tiagomac.resources.exception.FieldMessage;
 import com.tiagomac.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
 
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -29,6 +36,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
 		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
@@ -36,4 +48,6 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		}
 		return list.isEmpty();
 	}
+	
+	
 }
